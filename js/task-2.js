0b09945caec12fa0897487be01902f2dedad7d27
Johnny221 <jackmironenko@gmail.com>
2020-02-26 14:37:36 +0200

    hw-03

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+const countProps = function(obj) {
+  const numberOfValues = Object.values(obj);
+  return numberOfValues.length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
